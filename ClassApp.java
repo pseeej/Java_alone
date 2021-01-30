45752148386898fafc82efae3b4791b2de3d4819
pseeej <49185035+pseeej@users.noreply.github.com>
2021-01-31 00:00:11 +0900

    Delete ClassApp.java
    파일 위치 수정

--- ClassApp.java
+++
@@ -1,7 +0,0 @@
-public class ClassApp {
-    public static void main(String[] args) {
-        System.out.println(Math.PI);
-        System.out.println(Math.floor(1.6));    //숫자 내려주는 내장함수
-        System.out.println(Math.ceil(1.6)); //숫자 올림
-    }
-}
